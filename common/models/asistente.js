d4d54ea44ad9d488430c576b7ce18eebbbb4a0ea
Israel Garcia <igarcia@itexico.net>
2017-05-10 16:16:11 -0500

    first approach to remote methods and built-in validations

--- common/models/asistente.js
+++ common/models/asistente.js
@@ -2,4 +2,30 @@
 
 module.exports = function(Asistente) {
 
+    //Testing validations
+    Asistente.validatesPresenceOf('nombre','apellidop','apellidom','correo');
+
+    //This is not supposed to work on mysql but it does
+    Asistente.validatesUniquenessOf('nombre',{message:'Field should be unique'});
+
+    //Remote mehod attempt
+    Asistente.status=function(cb){
+        var response='Tiny Riiiiiiiiiiick!';
+        console.log('i got something! '+response);
+        cb(null,response);
+    };
+
+    Asistente.remoteMethod(
+        'status',{
+            http:{
+                path:'/status',
+                verb:'get'
+            },
+            returns:{
+                arg:'status',
+                type:'string'
+            }
+        }
+    );
+
 };
