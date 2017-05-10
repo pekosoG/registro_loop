648de6a733ec414530c00984747293b753ad0f0d
Israel Garcia <igarcia@itexico.net>
2017-05-10 11:47:55 -0500

    create tables based on models, done

---
+++ server/create-lb-tables.js
@@ -0,0 +1,20 @@
+/**
+ * In this file we run an aunto-migrate of the models that we have on our project
+ * 
+ *    +++++++++++++++++++++
+ *    ++     WARNING     ++
+ *    +++++++++++++++++++++
+ * 
+ * If you run auto-migrate in a model that already have the table, you will LOSE information,
+ * first you need to check if there's a table, otherwise you need to use autoupdate
+ * 
+ * ref: https://loopback.io/doc/en/lb2/Creating-database-tables-for-built-in-models.html
+ */
+var server = require('./server');
+var ds = server.dataSources.mysql;
+var lbTables = ['asistente', 'becas', 'industria', 'usuario'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
